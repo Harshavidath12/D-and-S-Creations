7f154b9546699e7177f786f94b516b927374eed8
Vidath Harshitha <harshithavidath@gmail.com>
2025-09-02 12:07:00 +0530

    Database connection
    I have connected the database

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+console.log("Byeeeeee");   
+//E1gMihrKg842U8Sd
+
+const mongoose=require("mongoose");
+const express = require("express");
+
+
+
+const app=express(); 
+
+
+
+//Middleware
+app.use("/",(req,res,next)=>{
+    res.send("Hello from express");
+});
+
+
+mongoose.connect("mongodb+srv://admin:E1gMihrKg842U8Sd@cluster0.sp3lpkf.mongodb.net/")
+.then(()=> console.log("Connected to MongoDB"))
+.then(()=>{
+    app.listen(5001);
+})
+.catch((err)=> console.log((err)));
