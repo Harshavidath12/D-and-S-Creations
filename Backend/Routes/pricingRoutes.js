3d0efcf57fb88d65332d1b126536e9c40a686f63
Vidath Harshitha <harshithavidath@gmail.com>
2025-09-19 21:47:20 +0530

    updated backend
    backend updated

--- Backend/Routes/pricingRoutes.js
+++ Backend/Routes/pricingRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 let pricing = {
-  Outdoor: { daily: 50000, extraHour: 1000 },
+  Outdoor: { daily: 50000, extraHour: 1000 },//pricing calculation
   Indoor: { daily: 24000, extraHour: 1000 },
   P3: { daily: 12000, extraHour: 1000 },
   P6: { daily: 6000, extraHour: 1000 },
@@ -14,7 +14,7 @@ router.get("/", (req, res) => {
   res.json(pricing);
 });
 
-// UPDATE pricing
+// UPDATED pricing
 router.put("/", (req, res) => {
   pricing = req.body;
   res.json({ message: "Pricing updated successfully!", pricing });
