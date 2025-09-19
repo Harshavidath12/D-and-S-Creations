575e90a4de61dbd599acaa3624c1485a3cd7f331
ChirathVihigum <134323576+ChirathVihigum@users.noreply.github.com>
2025-09-19 01:44:58 -0700

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,24 +1,19 @@
-console.log("Byeeeeee");   
-//E1gMihrKg842U8Sd
-
-const mongoose=require("mongoose");
 const express = require("express");
+const mongoose = require("mongoose");
 
+const app = express();
 
-
-const app=express(); 
-
-
-
-//Middleware
-app.use("/",(req,res,next)=>{
-    res.send("Hello from express");
+// Middleware
+app.use("/", (req, res, next) => {
+  res.send("Hello from express");
 });
 
-
-mongoose.connect("mongodb+srv://admin:E1gMihrKg842U8Sd@cluster0.sp3lpkf.mongodb.net/")
-.then(()=> console.log("Connected to MongoDB"))
-.then(()=>{
-    app.listen(5000);
-})
-.catch((err)=> console.log((err)));
+mongoose
+  .connect(
+    "mongodb+srv://vihigumchirath_db_user:93UQXuxJtn248xK2@cluster0.sp3lpkf.mongodb.net/test?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(5000, () => console.log("Server running on port 5000"));
+  })
+  .catch((err) => console.error(err));
