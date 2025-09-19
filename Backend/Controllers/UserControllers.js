4adf4a87c54682229443c650abd58acbc6db345c
Vidath Harshitha <harshithavidath@gmail.com>
2025-09-19 21:11:06 +0530

    my backend
    backend inserted

---
+++ Backend/Controllers/UserControllers.js
@@ -0,0 +1,128 @@
+//const User = require("../Model/UserModel");
+const User=require("../Model/RentalModel");
+
+// Data display part
+
+const getAllUsers = async (req, res, next) => {
+  let users;
+
+  // Get all users
+  try {
+    users = await User.find();
+  } catch (err) {
+    console.log(err);
+  }
+
+  // Not found
+  if (!users) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  // Display all users
+  return res.status(200).json({ users });
+};
+
+
+//Data Insert part
+
+const addUsers= async(req, res, next)=> {
+  const {name,ledBoardType,quantity,location,purpose,rentalStartDateTime,rentalEndDateTime,paymentMethod}=req.body;
+
+  let users;
+
+  try{
+    users=new User({name,ledBoardType,quantity,location,purpose,rentalStartDateTime,rentalEndDateTime,paymentMethod});
+    await users.save();//save to database
+  }catch(err){
+    console.log(err);
+  }
+  // if data is not getting inserted
+
+  if(!users){
+     return res.status(404).json({message:"unable to add users"});
+
+  }
+   return res.status(200).json({users});
+
+};
+
+//Get  by Id
+const getById= async(req,res,next)=>{
+    const id=req.params.id;
+
+    let user;
+
+    try{
+        user=await User.findById(id);// me id ekata me user inawada
+    }catch(err){
+        console.log(err);
+    }
+    // not available users
+    if(!user){
+     return res.status(404).json({message:"Users not found"});
+
+  }
+   return res.status(200).json({user});
+
+};
+
+
+
+//Update user details
+const updateUser=async(req,res,next)=>{
+   const id=req.params.id;
+
+    const {name,ledBoardType,quantity,location,purpose,rentalStartDateTime,rentalEndDateTime,paymentMethod}=req.body;
+
+    let users;
+    try{
+       users=await User.findByIdAndUpdate(id,
+
+       {name,ledBoardType,quantity,location,purpose,rentalStartDateTime,rentalEndDateTime,paymentMethod});
+       users=await users.save();
+    }catch(err){
+      console.log(err);
+    }
+     if(!users){
+     return res.status(404).json({message:"Unable to update user details"});
+
+  }
+   return res.status(200).json({users});
+};
+
+
+//Delete Uuser Details
+
+const deleteUser=async(req,res,next)=>{
+  const id=req.params.id;
+
+  let user;// creating a variable
+
+  try{
+    user=await User.findByIdAndDelete(id) // this will get the id id and delete the data belong to that id
+  }catch(err){
+    console.log(err);
+  }
+
+   if(!user){
+     return res.status(404).json({message:"Ubale to delete user details"});
+
+  }
+   return res.status(200).json({user});
+};
+
+
+
+exports.getAllUsers = getAllUsers;
+
+exports.addUsers=addUsers;
+
+exports.getById=getById;
+
+exports.updateUser=updateUser;
+
+exports.deleteUser=deleteUser;
+
+
+
+
---
+++ Backend/Routes/pricingRoutes.js
@@ -0,0 +1,23 @@
+// routes/pricingRoutes.js
+const express = require('express');
+const router = express.Router();
+
+let pricing = {
+  Outdoor: { daily: 50000, extraHour: 1000 },
+  Indoor: { daily: 24000, extraHour: 1000 },
+  P3: { daily: 12000, extraHour: 1000 },
+  P6: { daily: 6000, extraHour: 1000 },
+};
+
+// GET current pricing
+router.get("/", (req, res) => {
+  res.json(pricing);
+});
+
+// UPDATE pricing
+router.put("/", (req, res) => {
+  pricing = req.body;
+  res.json({ message: "Pricing updated successfully!", pricing });
+});
+
+module.exports = router;
