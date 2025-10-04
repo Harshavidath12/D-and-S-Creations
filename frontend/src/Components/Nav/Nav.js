4ecf3113c4f75c56ebbd70c2ca4dc496fc65d6ed
Vidath Harshitha <harshithavidath@gmail.com>
2025-10-04 21:04:57 +0530

    Added Rashalis files
    Added Rashais files to main but there is an error in api.js

--- frontend/src/Components/Nav/Nav.js
+++ frontend/src/Components/Nav/Nav.js
@@ -1,56 +1,64 @@
-import React from 'react';
-import './nav.css'
-import {Link} from "react-router-dom";
+import React from "react";
+import "./nav.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <div>
       <ul className="home-url">
         <li className="home-ll">
-            <Link to="/mainhome" className="active home-a">
+          <Link to="/mainhome" className="active home-a">
             <h1>Home</h1>
-            </Link>
+          </Link>
         </li>
         <li className="home-ll">
-             <Link to="/adduser" className="active home-a">
+          <Link to="/adduser" className="active home-a">
             <h1>ADD user</h1>
-             </Link>
+          </Link>
         </li>
         <li className="home-ll">
-            <Link to="/userdetails" className="active home-a">
+          <Link to="/userdetails" className="active home-a">
             <h1>user details</h1>
-             </Link>
+          </Link>
         </li>
 
         <li className="home-ll">
-            <Link to="/conus" className="active home-a">
-            <h1>Contact Us</h1>
-             </Link>
-        </li> 
-
-          <li className="home-ll">
-            <Link to="/imgpart" className="active home-a">
-            <h1>Photos</h1>
-             </Link>
-           </li>
-
-           <li className="home-ll">
-          
-            <Link to="/ledboard" className="active home-a">
-             <h1>LED Board</h1>
-             </Link>
+          <Link to="/client-designers" className="active home-a">
+            <h1>Designers</h1>
+          </Link>
         </li>
 
-         <li> 
-                    <Link to="/Login" className="nav-link">Login</Link> 
-                </li>
-                <li> 
-                    <Link to="/Register" className="nav-link">Register</Link> 
-                </li>
-        
+        <li className="home-ll">
+          <Link to="/complaints" className="active home-a">
+            <h1>Contact Us</h1>
+          </Link>
+        </li>
+
+        <li className="home-ll">
+          <Link to="/imgpart" className="active home-a">
+            <h1>Photos</h1>
+          </Link>
+        </li>
+
+        <li className="home-ll">
+          <Link to="/ledboard" className="active home-a">
+            <h1>LED Board</h1>
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/Login" className="nav-link">
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link to="/Register" className="nav-link">
+            Register
+          </Link>
+        </li>
       </ul>
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
